9f9c94ee71d5c3b711a972da9252ba1142939bd5
André Pina <andredspina@gmail.com>
2025-05-10 22:35:53 +0100

    C# Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {// Name: André Duarte Santos de Pina
+     // Class: CSE 210: Programming with Classes
+     // Exercise: C# Programming Exercise 3: Loops
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Go Higher!");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it, Congrats!!!");
+            }
+
+        }
     }
 }
