4990e6f45df0c8beff62f846740aefee0436d34b
André Pina <andredspina@gmail.com>
2025-05-10 12:14:01 +0100

    Finished C# Programming Exercise 1: Input and Output

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Name: André Duarte Santos de Pina
+        // Class: CSE 210: Programming with Classes
+        // Exercise: C# Programming Exercise 1: Input and Output
+        // This program will ask for the user's first name and print a greeting message.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Print a greeting message using the user's first and last name
+        // The message should be: "Hello, [first name] [last name]! Welcome to C# programming."
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+
     }
 }
